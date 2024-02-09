e328554eae3a56ab0e51520361f7c4334e87ede5
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-02-09 17:23:03 +0100

    Creazione intervallo e tempo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 const countDownDate = new Date("Feb 12, 2024 9:30:00").getTime();
 
+const intervalloTempo = setInterval(function () {
+
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let giorni = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let ore = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minuti = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let secondi = Math.floor((distance % (1000 * 60)) / 1000);
+
+}, 1000);
